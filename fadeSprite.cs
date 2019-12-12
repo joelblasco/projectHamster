c2c30c4e7e8d0a2716ad35988986bec88ab8140f
joelblasco <joelblasco4@gmail.com>
2019-12-12 23:51:50 +0100

    Entrega2
    fadeScript: Permite interactuar con este objeto de manera que al ponerse encima se transparenta un poco para poder buscar al jugador debajo de ciertos sprites en los que pueda estar.
    
    playerController: actualizado, ahora permite escalar paredes como otra interacción adicional.

---
+++ fadeSprite.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Este script irá en cada objeto tras el que el hamster pueda entrar y hará que el objeto se torne medio transparente para así ver si está el enemigo dentro.
+ * 
+ */
+public class fadeSprite : MonoBehaviour
+{
+    SpriteRenderer sr;
+    bool transparent;
+    public float fadeSpeed;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            fade();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            fade();
+        }
+
+    }
+    void fade()
+    {
+        if (!transparent)
+        {
+            transparent = true;
+            StartCoroutine(fadeOut());
+        }
+        else
+        {
+            transparent = false;
+            StartCoroutine(fadeIn());
+        }
+    }
+    IEnumerator fadeIn() //volver a normal
+    {
+        while (sr.color.a < 0.99f)
+        {
+            Color newColor = sr.color;
+            newColor.a += fadeSpeed * Time.deltaTime;
+            sr.color = newColor;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    IEnumerator fadeOut() //volver transparente
+    {
+        while (sr.color.a > 0.5f)
+        {
+            Color newColor = sr.color;
+            newColor.a -= fadeSpeed * Time.deltaTime;
+            sr.color = newColor;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
--- playerController.cs
+++ playerController.cs
@@ -7,17 +7,20 @@ using UnityEngine;
  * Adicionalmente podrá escalar la cama y objetos parecidos (?)
  * De momento solo se movera en un eje.
  */
-public class playerController : MonoBehaviour {
+public class playerController : MonoBehaviour
+{
     public float speed, jumpForce, height;
     Animator myAnim; //to do
     Rigidbody rb;
     public bool grounded, isEnemy;
-	void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         grounded = false;
         RaycastHit hit;
         Debug.DrawRay(transform.position, Vector3.down * height, Color.green);
@@ -29,11 +32,21 @@ public class playerController : MonoBehaviour {
             }
         }
 
-        if (grounded && Input.GetKeyDown(KeyCode.S))
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
         }
         float motion = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(motion, 0, 0);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "climber")
+        {
+            if(!grounded && Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * jumpForce/2 * Time.deltaTime, ForceMode.VelocityChange);
+            }
+        }
     }
 }
